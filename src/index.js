5ef5d251e601b55ec8f379f70e53a38f155d23d4
Nathan Kidd <nathanblenheimkidd@gmail.com>
2025-08-02 19:58:50 +0100

    Created repo

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+  async fetch(request) {
+    // Prepare the 418 response body and headers
+    const responseBody = JSON.stringify({
+      status: 418,
+      message: "I'm a teapot. Short and stout.",
+      method: request.method,
+      url: request.url
+    }, null, 2);
+
+    const responseHeaders = new Headers({
+      "Content-Type": "application/json; charset=utf-8",
+      "X-Teapot": "Yes, really",
+      "Cache-Control": "no-store"
+    });
+
+    return new Response(responseBody, {
+      status: 418,
+      statusText: "I'm a teapot",
+      headers: responseHeaders
+    });
+  }
 };
